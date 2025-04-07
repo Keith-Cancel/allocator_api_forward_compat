15b965e9b0b01fa871fa6e52f8fd685be8d98647
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-04-06 22:37:36 -0700

    Update lib.rs

--- store_derive/src/lib.rs
+++ store_derive/src/lib.rs
@@ -8,7 +8,7 @@ pub fn derive_store(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
-        alloc_store_traits::macros::__impl_store!(#name);
+        api_traits::macros::__impl_store!(#name);
     };
     TokenStream::from(expanded)
 }
@@ -19,7 +19,7 @@ pub fn derive_store_dangling(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
-        alloc_store_traits::macros::__impl_store_dangling!(#name);
+        api_traits::macros::__impl_store_dangling!(#name);
     };
     TokenStream::from(expanded)
 }
@@ -30,7 +30,7 @@ pub fn derive_store_stable(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
-        alloc_store_traits::macros::__impl_store_stable!(#name);
+        api_traits::macros::__impl_store_stable!(#name);
     };
     TokenStream::from(expanded)
 }
@@ -41,7 +41,7 @@ pub fn derive_store_no_op_resolve(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
-        alloc_store_traits::macros::__impl_store_no_op_resolve!(#name);
+        api_traits::macros::__impl_store_no_op_resolve!(#name);
     };
     TokenStream::from(expanded)
 }
@@ -52,7 +52,7 @@ pub fn derive_store_pinning(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
-        alloc_store_traits::macros::__impl_store_pinning!(#name);
+        api_traits::macros::__impl_store_pinning!(#name);
     };
     TokenStream::from(expanded)
 }
